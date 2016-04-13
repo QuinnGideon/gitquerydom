768ae65cc18dd2a66ffe70728b18dc1cd71601a8
Quinn Gideon <Quinn.Gideon@gmail.com>
2016-04-13 10:07:58 -0400

    Init commit, and adding other files for project

---
+++ oion.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+	$("h1").animate({
+		fontSize: "60px"
+	},
+	2000);
+
+	$("h4").animate({
+		marginLeft: "50px"
+	},
+	2000);
+
+	$("#obama").animate({
+		width: "0px",
+		height: "0px",
+	},10000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
---
+++ scripts.js
@@ -0,0 +1,19 @@
+// $(document).ready(function(){
+// 	$("#button").click(function(){
+// 		var date= $("#input1").val(); // How to get input
+
+// 	});
+
+
+
+
+
+
+
+
+// });
+
+
+// document.getElementById('input1').value{
+// 	console.log('input1');
+// }
