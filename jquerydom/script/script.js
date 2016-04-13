ee58c77133dc24fc884264c1eafa1d4c64406413
Giovanna Moraes <g.vaz.guimaraes@gmail.com>
2016-04-13 18:24:27 -0400

    adding username email check and fixing bugs

--- jquerydom/script/script.js
+++ jquerydom/script/script.js
@@ -3,6 +3,16 @@ $(document).ready(function(){
 
 // to check outside of scope, assign the function to a variable name (no var)
 
+// function below will check user email 
+
+function validate() {
+			var x = $('#username').val()
+        var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+        //var address = document.getElementById[email].value;
+        if (reg.test(x) == true) 
+        	return true
+};
+
 // function below will check password 
 function passCheck(){
 		if ($('#password').val()=== "12345"){
@@ -44,21 +54,22 @@ function rightFormat(){
 function submitCheck(){
 	$('form').submit(function(){
 	//alert("I've been clicked")
-	if (dateSplit() === true && rightFormat() ===true && passCheck()===true){
+	if (validate()===true && dateSplit() === true && rightFormat() ===true && passCheck()===true){
 		alert('logging in soon')
-	} else if (passCheck()===false){
+	} else if (validate()===true && passCheck()===false && rightFormat()===true && dateSplit()===true){
 		alert("WRONG PASSWORD! 2 more tries before self combustion")
-	} else if (rightFormat()===false){
+	} else if (rightFormat()===false && dateSplit()===false && passCheck()===true){
 		alert ("date format must be in mm/dd/yyyy")
-	} else if (dateSplit() === false){
+	} else if (dateSplit() === false && passCheck()===true && rightFormat()===true){
 		alert('Invalid date!')
 	} else {
 		alert ("one or more inputs is incorrect, please try again!")
 	}
+})
+};
 
-	})
-}
 submitCheck()
+
 })
 
 
